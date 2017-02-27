f89e3678f30b95092e06899f854cfbc64c0f55f9
Waqar Wk6 Bushra <wk6@Waqars-MacBook-Pro.local>
2017-02-27 15:21:45 -0500

    initial commit

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=wk6;
+    $username = 'wk6';
+    $password = 'dryden49';
 
     try {
         $db = new PDO($dsn, $username, $password);
